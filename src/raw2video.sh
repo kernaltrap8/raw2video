70d090ae22ba6a91b86d67fa4b1a73558aa527c2
kernaltrap8 <kernaltrap@gmail.com>
2024-06-24 22:41:28 -0500

    final script version
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/raw2video.sh
+++ src/raw2video.sh
@@ -15,13 +15,28 @@ RAW_INPUT="$1"
 AUDIO_OUTPUT="$2"
 FINAL_OUTPUT="$3"
 
+if [[ $AUDIO_OUTPUT != *.flac ]]; then
+	AUDIO_OUTPUT="$2.flac"
+fi
+
+if [[ $FINAL_OUTPUT != *.mp4  ]]; then
+	FINAL_OUTPUT="$3.mp4"
+fi
+
+if [ $(du -b $RAW_INPUT | cut -f1) -lt 1000000  ]; then
+	echo "Input file is too small."
+	exit 1
+fi
+
 echo "Creating audio stream..."
-sleep 2
+sleep 1
 ffmpeg -f u8 -ar 44100 -i "$RAW_INPUT" -c:a flac "$AUDIO_OUTPUT"
 echo "Done."
-sleep 2
+sleep 1
 echo "Creating video stream and including audio..."
+sleep 1
 ffmpeg -f rawvideo -pix_fmt rgb24 -s 1920x1080 -i "$RAW_INPUT" -i "$AUDIO_OUTPUT" -c:v libx264 -c:a aac -b:a 192k "$FINAL_OUTPUT"
 echo "Done."
+sleep 1
 echo "Removing old files..."
 rm -vf $2 
